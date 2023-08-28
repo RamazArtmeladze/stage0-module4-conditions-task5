b68271b6ecc309cee6da9b2d632107d5c393572a
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-29 01:50:39 +0400

    update

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int isLeap = 0;
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            isLeap = 1;
+        }
+
+        switch (isLeap) {
+            case (1):
+                System.out.println(year + " is a leap year");
+                break;
+            case (0):
+                System.out.println(year + " is not a leap year");
+                break;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        int days;
+
+        switch (month) {
+            case 2: // February
+                days = 28; // Assuming non-leap year
+                break;
+            case 4: // April
+            case 6: // June
+            case 9: // September
+            case 11: // November
+                days = 30;
+                break;
+            default: // January, March, May, July, August, October, December
+                days = 31;
+                break;
+        }
+
+        System.out.println( days);
     }
 }
+
