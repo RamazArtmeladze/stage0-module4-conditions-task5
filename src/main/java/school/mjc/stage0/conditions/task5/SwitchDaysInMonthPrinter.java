a5f015fa6ee1f08ac1b7d0c21a18617f9c9ed850
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-29 01:58:19 +0400

    update

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -14,12 +14,18 @@ public class SwitchDaysInMonthPrinter {
             case 11: // November
                 days = 30;
                 break;
-            default: // January, March, May, July, August, October, December
-                days = 31;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                days = 31; // January, March, May, July, August, October, December
+            default:
+                System.out.println( "wrong number!");
                 break;
         }
-
-        System.out.println( days);
     }
 }
 
