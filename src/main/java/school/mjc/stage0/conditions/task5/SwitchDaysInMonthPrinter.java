a40dc5218fbbc30da08de0b68b615ade3fc1c388
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-29 02:08:46 +0400

    update

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,17 +2,16 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
-        int days;
 
         switch (month) {
             case 2: // February
-                days = 28; // Assuming non-leap year
+                System.out.println(28);
                 break;
             case 4: // April
             case 6: // June
             case 9: // September
             case 11: // November
-                days = 30;
+                System.out.println(30);
                 break;
             case 1:
             case 3:
@@ -21,7 +20,7 @@ public class SwitchDaysInMonthPrinter {
             case 8:
             case 10:
             case 12:
-                days = 31; // January, March, May, July, August, October, December
+                System.out.println(31);
                 break;
             default:
                 System.out.println( "wrong number!");
